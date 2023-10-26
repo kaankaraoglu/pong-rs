890e99a3c0831060c5a286ed904aae64cc9e735f
Kaan Karaoglu <kkaraogl@volvocars.com>
2023-10-26 18:47:10 +0200

    Add key event handlers and player paddle

--- src/main.rs
+++ src/main.rs
@@ -4,43 +4,67 @@ use std::path::PathBuf;
 use std::{env, path};
 
 use ggez::glam::{vec2, Vec2};
-use ggez::graphics::Color;
+use ggez::graphics::{Color, Rect};
+use ggez::input::keyboard::{KeyCode, KeyInput};
 use ggez::{event, graphics, Context, GameError, GameResult};
 use graphics::Canvas;
+
+use ball::Ball;
+use paddle::Paddle;
+
+mod ball;
+mod paddle;
 
 struct MainState {
     frames: usize,
     ball: Ball,
-}
-
-struct Ball {
-    position: Vec2,
-    mesh: graphics::Mesh,
-    speed: f32,
-    direction: Vec2,
-}
-
-impl Ball {
-    const RADIUS: f32 = 10.0;
+    player_paddle: Paddle,
 }
 
 impl MainState {
     fn new(ctx: &mut Context) -> GameResult<MainState> {
+        const PLAYER_PADDLE_STARTING_POSITION_X_OFFSET: f32 = 20.0;
+        let (_width, height) = ctx.gfx.drawable_size();
+
+        // Create the ball
         let ball = Ball {
             position: vec2(50.0, 50.0),
             mesh: graphics::Mesh::new_circle(
                 ctx,
                 graphics::DrawMode::fill(),
-                vec2(0.0, 0.0),
+                Vec2::ZERO,
                 Ball::RADIUS,
-                0.1,
+                0.2,
                 Color::RED,
             )?,
             speed: 4.5,
             direction: vec2(1.0, 0.75),
         };
 
-        Ok(MainState { frames: 0, ball })
+        let paddle_mesh = graphics::Mesh::new_rectangle(
+            ctx,
+            graphics::DrawMode::fill(),
+            Rect {
+                x: 0.0,
+                y: 0.0,
+                w: Paddle::WIDTH,
+                h: Paddle::HEIGHT,
+            },
+            Color::WHITE,
+        )?;
+
+        // Create player's paddle
+        let player_paddle = Paddle {
+            position: vec2(PLAYER_PADDLE_STARTING_POSITION_X_OFFSET, height / 2.0),
+            mesh: paddle_mesh,
+            speed: 50.0,
+        };
+
+        Ok(MainState {
+            frames: 0,
+            ball,
+            player_paddle,
+        })
     }
 }
 
@@ -67,10 +91,15 @@ impl event::EventHandler<GameError> for MainState {
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         let mut canvas = Canvas::from_frame(ctx, graphics::Color::from([0.1, 0.2, 0.3, 1.0]));
 
+        // Draw the ball
         canvas.draw(
             &self.ball.mesh,
             Vec2::new(self.ball.position.x, self.ball.position.y),
         );
+
+        // Draw player's paddle
+        canvas.draw(&self.player_paddle.mesh, self.player_paddle.position);
+
         canvas.finish(ctx)?;
 
         // Count FPS and log.
@@ -79,6 +108,48 @@ impl event::EventHandler<GameError> for MainState {
             println!("FPS: {}", ctx.time.fps().ceil());
         }
 
+        Ok(())
+    }
+
+    fn key_down_event(
+        &mut self,
+        ctx: &mut Context,
+        input: KeyInput,
+        _repeated: bool,
+    ) -> GameResult {
+        let (_screen_width, screen_height) = ctx.gfx.drawable_size();
+        const EDGE_OFFSET: f32 = 15.0;
+
+        match input.keycode {
+            Some(KeyCode::Up) => {
+                let mut next_pos = vec2(
+                    0.0,
+                    self.player_paddle.position.y - self.player_paddle.speed,
+                );
+
+                if next_pos.y <= 0.0 {
+                    next_pos.y = 0.0 + EDGE_OFFSET
+                }
+
+                self.player_paddle.position.y = next_pos.y;
+            }
+            Some(KeyCode::Down) => {
+                let mut next_pos = vec2(
+                    0.0,
+                    self.player_paddle.position.y + self.player_paddle.speed,
+                );
+
+                if next_pos.y + Paddle::HEIGHT >= screen_height {
+                    next_pos.y = screen_height - Paddle::HEIGHT - EDGE_OFFSET
+                }
+
+                self.player_paddle.position.y = next_pos.y;
+            }
+            Some(KeyCode::Escape) => {
+                ctx.request_quit();
+            }
+            _ => (), // Do nothing
+        }
         Ok(())
     }
 }
